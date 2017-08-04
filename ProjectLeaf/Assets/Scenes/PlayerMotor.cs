efee6f971ec7a59ff8f6d1955667b7649acaad48
Jlevine <fundragonstudios@gmail.com>
2017-08-03 21:52:46 -0700

    VortexJump, Verticalmove, Camera follow
    Created player movement and a camera script that follows the player tags

---
+++ ProjectLeaf/Assets/Scenes/PlayerMotor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+
+    private float verticalVelocity;
+    private float gravity = 5.5f;
+    private float jumpForce = 10.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        moveVector.x = Input.GetAxis("Horizontal") * 5.0f;
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
